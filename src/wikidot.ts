8d2da51cc171da3d2641897d260fb95718ea3470
ZreXoc <zeexoc@outlook.com>
2021-12-04 18:52:15 +0800

    add some suggestions

--- src/wikidot.ts
+++ src/wikidot.ts
@@ -1,3 +1,4 @@
+import * as _ from 'lodash';
 import { CancellationToken, editor, languages, Position } from 'monaco-editor';
 import { range } from './util';
 
@@ -12,7 +13,7 @@ type InlineBlock = {
   ) => languages.CompletionItem | languages.CompletionItem[];
 };
 
-const inlineStyle = {
+const inlineMarks = {
   bold: {
     mark: '**',
   },
@@ -27,72 +28,129 @@ const inlineStyle = {
   },
 };
 
-const inlineBlock: {
-  [key: string]: InlineBlock;
-} = {
-  size: {
-    close: '[[/size]]',
-    suggestion(model, position) {
-      const before = model.getValueInRange(range.l.before(model, position));
-      const wordRange = range.w.c(model, position);
-      // const word = model.getValueInRange(wordRange);
+export const completionItemProvider: languages.CompletionItemProvider = {
+  provideCompletionItems(model, position) {
+    let suggestions: Array<
+      languages.CompletionItem | languages.CompletionItem[]
+    > = [];
+    const before = model.getValueInRange(range.l.before(model, position));
+    const wordRange = range.w.c(model, position);
+    const word = model.getValueInRange(wordRange);
 
-      if (before.endsWith('[['))
-        return {
-          kind: languages.CompletionItemKind.Constant,
-          range: wordRange,
-          insertText: 'size ${1:0.8}em',
-          insertTextRules:
-            languages.CompletionItemInsertTextRule.InsertAsSnippet,
-          label: 'size',
-          detail: 'change the size of the text inside',
-          additionalTextEdits: [
-            {
-              range: {
-                ...wordRange,
-                startColumn: model.getLineLastNonWhitespaceColumn(
-                  position.lineNumber
-                ),
-                endColumn: model.getLineLastNonWhitespaceColumn(
-                  position.lineNumber
-                ),
-              },
-              text: inlineBlock.size.close + '',
+    console.log(before, word);
+
+    switch (before[before.length - 1]) {
+      //blocks
+      case '#': {
+        suggestions.push(
+          [
+            [
+              'color',
+              '##${1:color}|${2:text}##',
+              'change the size of the text',
+            ],
+            [
+              'size',
+              '[[size ${1:0.8}em]]${2:text}[[/size]]',
+              'change the size of the text',
+            ],
+            ['h1', '\n+ ${1}', 'header one'],
+            ['h2', '\n++ ', 'header two'],
+            ['h3', '\n+++ ', 'header three'],
+            ['h4', '\n++++ ', 'header four'],
+            ['h5', '\n+++++ ', 'header five'],
+            ['h6', '\n++++++ ', 'header six'],
+            ['quote', '\n> ', 'quote'],
+            [
+              'collapsible',
+              '[[collapsible show="+ ${1:show}" hide="- ${2:hide}"]]\n${3:inside}\n[[/collapsible]]',
+              'collapsible block',
+            ],
+          ].map(([label, insertText, detail]) => {
+            return {
+              kind: languages.CompletionItemKind.Constant,
+              range: wordRange,
+              insertText,
+              insertTextRules:
+                languages.CompletionItemInsertTextRule.InsertAsSnippet,
+              label,
+              detail,
+              additionalTextEdits: [
+                {
+                  range: {
+                    ...wordRange,
+                    startColumn: wordRange.startColumn - 1,
+                    endColumn: wordRange.startColumn,
+                  },
+                  text: '',
+                },
+              ], //delete the character '#' before
+            };
+          })
+        );
+      }
+    }
+
+    if (before.endsWith('[[')) {
+      //block:size
+      suggestions.push({
+        kind: languages.CompletionItemKind.Constant,
+        range: wordRange,
+        insertText: 'size ${1:0.8}em',
+        insertTextRules: languages.CompletionItemInsertTextRule.InsertAsSnippet,
+        label: 'size',
+        detail: 'change the size of the text inside',
+        /* additionalTextEdits: [
+          {
+            range: {
+              ...wordRange,
+              startColumn: model.getLineLastNonWhitespaceColumn(
+                position.lineNumber
+              ),
+              endColumn: model.getLineLastNonWhitespaceColumn(
+                position.lineNumber
+              ),
             },
-          ],
-        };
+            text: '[[size]]',
+          },
+        ], */
+      });
 
       if (before.search(/\[\[size\s+$/) > -1) {
         /*  TODO: not work   
-      const textSize = word.match(/^[0-9]+/);
+    const textSize = word.match(/^[0-9]+/);
 
-      if (textSize)
-        return ['px', 'em'].map((s) => {
-          return {
-            insertText: textSize + s,
-            kind: languages.CompletionItemKind.Unit,
-            label: s,
-            range: wordRange,
-          };
-        });
- */
-        return ['xx-small', 'x-small', 'large', 'x-large', 'xx-large'].map(
-          (s) => {
+    if (textSize)
+      return ['px', 'em'].map((s) => {
+        return {
+          insertText: textSize + s,
+          kind: languages.CompletionItemKind.Unit,
+          label: s,
+          range: wordRange,
+        };
+      });
+*/
+        suggestions.push(
+          ['xx-small', 'x-small', 'large', 'x-large', 'xx-large'].map((s) => {
             return {
               kind: languages.CompletionItemKind.Constant,
               range: wordRange,
               insertText: s,
               label: s,
             };
-          }
+          })
         );
       }
-    },
+    }
+
+    return {
+      suggestions: _.flatten(suggestions),
+    };
   },
+  triggerCharacters: ['[', '$', '#', '\n'],
 };
 
 export const WIKIDOT = {
   id: 'wikidot',
-  inlineStyle,
-  inlineBlock: inlineBlock,
+  inlineMarks,
 };
